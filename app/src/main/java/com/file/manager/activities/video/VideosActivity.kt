1e1f6fae62d024791abe875ddeb14f4840621aa3
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-27 16:30:20 +0700

    :recycle: Refactor `VideosActivity` and :memo: Update `AndroidManifest.xml`

---
+++ app/src/main/java/com/file/manager/activities/video/VideosActivity.kt
@@ -0,0 +1,57 @@
+package com.file.manager.activities.video
+
+import android.content.pm.PackageManager
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.file.manager.databinding.ActivityVideosBinding
+import com.file.manager.utils.Utils.storagePermission
+import com.module.core.base.BaseActivity
+
+class VideosActivity : BaseActivity<ActivityVideosBinding>() {
+    override fun inflateViewBinding(inflater: LayoutInflater): ActivityVideosBinding {
+        return ActivityVideosBinding.inflate(inflater)
+    }
+
+    // Khởi tạo launcher để yêu cầu quyền
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            // Quyền được cấp
+            Toast.makeText(this, "Quyền được cấp thành công!", Toast.LENGTH_SHORT).show()
+        } else {
+            // Quyền bị từ chối
+            Toast.makeText(this, "Quyền bị từ chối!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun initView() {
+        requestStoragePermission(storagePermission)
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initListener() {
+
+    }
+
+    private fun requestStoragePermission(permission: String) {
+        when {
+            // Kiểm tra xem quyền đã được cấp chưa
+            checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED -> {
+                Toast.makeText(this, "Quyền đã được cấp!", Toast.LENGTH_SHORT).show()
+            }
+            // Nếu cần giải thích lý do yêu cầu quyền (người dùng đã từ chối trước đó)
+            shouldShowRequestPermissionRationale(permission) -> {
+                Toast.makeText(this, "Ứng dụng cần quyền này để truy cập bộ nhớ!", Toast.LENGTH_LONG).show()
+                // Sau khi giải thích, yêu cầu lại quyền
+                requestPermissionLauncher.launch(permission)
+            }
+            // Yêu cầu quyền lần đầu
+            else -> {
+                requestPermissionLauncher.launch(permission)
+            }
+        }
+    }
+}
