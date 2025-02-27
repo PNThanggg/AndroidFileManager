c48255248948942433b7a29226ace19ee6837263
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-27 18:04:52 +0700

    :recycle: Refactor ShortcutAdapter to use `setDataList` instead of `addDataList`.

--- app/src/main/java/com/file/manager/activities/video/ShortcutAdapter.kt
+++ app/src/main/java/com/file/manager/activities/video/ShortcutAdapter.kt
@@ -6,40 +6,11 @@ import com.file.manager.activities.video.models.ShortcutItem
 import com.file.manager.databinding.ItemShortcutBinding
 import com.module.core.base.BaseAdapterRecyclerView
 
-//class ShortcutAdapter(
-//    private val shortcuts: List<ShortcutItem>
-//) : RecyclerView.Adapter<ShortcutAdapter.ShortcutViewHolder>() {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShortcutViewHolder {
-//        val view =
-//            LayoutInflater.from(parent.context).inflate(R.layout.item_shortcut, parent, false)
-//        return ShortcutViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: ShortcutViewHolder, position: Int) {
-//        val shortcut = shortcuts[position]
-//        holder.bind(shortcut)
-//    }
-//
-//    override fun getItemCount(): Int = shortcuts.size
-//
-//    class ShortcutViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        private val icon: ImageView = itemView.findViewById(R.id.shortcut_icon)
-//        private val text: TextView = itemView.findViewById(R.id.shortcut_text)
-//
-//        fun bind(shortcut: ShortcutItem) {
-//            icon.setImageResource(shortcut.iconResId)
-//            text.setText(shortcut.textResId)
-//            itemView.setOnClickListener { shortcut.onClick() }
-//        }
-//    }
-//}
-
 class ShortcutAdapter(
     shortcuts: List<ShortcutItem>
 ) : BaseAdapterRecyclerView<ShortcutItem, ItemShortcutBinding>() {
     init {
-        addDataList(shortcuts)
+        setDataList(shortcuts)
     }
 
     override fun inflateBinding(inflater: LayoutInflater, parent: ViewGroup): ItemShortcutBinding {
