a8d84e0a9752c789b6e9068a1e679a7746b304c6
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-27 11:23:09 +0700

    :zap: Add Hilt's `@AndroidEntryPoint` to `SplashActivity`

--- app/src/main/java/com/file/manager/SplashActivity.kt
+++ app/src/main/java/com/file/manager/SplashActivity.kt
@@ -1,10 +1,11 @@
 package com.file.manager
 
-
 import android.view.LayoutInflater
 import com.file.manager.databinding.ActivitySplashBinding
 import com.module.core.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashActivity : BaseActivity<ActivitySplashBinding>() {
     override fun inflateViewBinding(inflater: LayoutInflater): ActivitySplashBinding {
         return ActivitySplashBinding.inflate(inflater)
