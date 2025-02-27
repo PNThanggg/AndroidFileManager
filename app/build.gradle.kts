4c03985f5094c7e42bc04905d367c819d477d857
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-27 10:45:27 +0700

    :wrench: Add `:Core:Common` and `:Core:Datastore` as dependencies to the app module.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,17 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(project(":Core:Common"))
+    implementation(project(":Core:Datastore"))
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
