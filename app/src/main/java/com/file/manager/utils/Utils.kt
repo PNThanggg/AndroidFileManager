5f06133c49a08ccb730002025a823f063804bb26
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-27 16:06:58 +0700

    :sparkles: Add `Utils` object to handle storage permission based on Android version.

---
+++ app/src/main/java/com/file/manager/utils/Utils.kt
@@ -0,0 +1,13 @@
+package com.file.manager.utils
+
+import android.Manifest
+import com.module.core.utils.isSdkR
+import com.module.core.utils.isSdkTIRAMISU
+
+object Utils {
+    val storagePermission = when {
+        isSdkTIRAMISU() -> Manifest.permission.READ_MEDIA_VIDEO
+        isSdkR() -> Manifest.permission.READ_EXTERNAL_STORAGE
+        else -> Manifest.permission.WRITE_EXTERNAL_STORAGE
+    }
+}
