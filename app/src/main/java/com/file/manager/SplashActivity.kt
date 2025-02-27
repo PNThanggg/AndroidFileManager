10ff7386eb26e0fdf7222c6cde00c6671dd43440
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-27 11:44:10 +0700

    :recycle: Refactor SplashActivity to collect application preferences using lifecycleScope and redirect to Intro or Main activity based on `firstLaunch` preference.

--- app/src/main/java/com/file/manager/SplashActivity.kt
+++ app/src/main/java/com/file/manager/SplashActivity.kt
@@ -1,9 +1,14 @@
 package com.file.manager
 
+import android.content.Intent
 import android.view.LayoutInflater
+import androidx.lifecycle.lifecycleScope
 import com.file.manager.databinding.ActivitySplashBinding
 import com.module.core.base.BaseActivity
+import com.modules.core.datastore.repository.PreferencesRepository
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity : BaseActivity<ActivitySplashBinding>() {
@@ -11,7 +16,26 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>() {
         return ActivitySplashBinding.inflate(inflater)
     }
 
+    @Inject
+    lateinit var preferencesRepository: PreferencesRepository
+
     override fun initView() {
+        lifecycleScope.launch {
+            preferencesRepository.applicationPreferences.collect { prefs ->
+//                delay(1500)
+
+                if (prefs.firstLaunch) {
+                    startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+                } else {
+                    startActivity(
+                        Intent(
+                            this@SplashActivity, MainActivity::class.java
+                        )
+                    )
+                }
+                finish()
+            }
+        }
     }
 
     override fun initData() {
