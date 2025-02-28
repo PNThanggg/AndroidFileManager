a9ba127868135bf3a436bf388ab10662ba843821
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-28 12:07:14 +0700

    :sparkles: Add `ApplicationPreferences` data class to store app preferences

--- Core/Datastore/src/main/java/com/modules/core/datastore/models/ApplicationPreferences.kt
+++ Core/Datastore/src/main/java/com/modules/core/datastore/models/ApplicationPreferences.kt
@@ -1,8 +1,29 @@
 package com.modules.core.datastore.models
 
+import com.modules.core.model.MediaViewMode
+import com.modules.core.model.Sort
+import com.modules.core.model.ThemeConfig
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ApplicationPreferences(
     val firstLaunch: Boolean = true,
+    val sortBy: Sort.By = Sort.By.TITLE,
+    val sortOrder: Sort.Order = Sort.Order.ASCENDING,
+    val themeConfig: ThemeConfig = ThemeConfig.SYSTEM,
+    val useHighContrastDarkTheme: Boolean = false,
+    val useDynamicColors: Boolean = true,
+    val markLastPlayedMedia: Boolean = true,
+    val showFloatingPlayButton: Boolean = true,
+    val excludeFolders: List<String> = emptyList(),
+    val mediaViewMode: MediaViewMode = MediaViewMode.FOLDERS,
+
+    // Fields
+    val showDurationField: Boolean = true,
+    val showExtensionField: Boolean = false,
+    val showPathField: Boolean = true,
+    val showResolutionField: Boolean = false,
+    val showSizeField: Boolean = false,
+    val showThumbnailField: Boolean = true,
+    val showPlayedProgress: Boolean = true,
 )
