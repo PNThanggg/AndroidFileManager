a406a7d03f7287dde529203de5f41dea2d6f0c75
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-03-03 16:57:20 +0700

    :sparkles: Add constants for file manager sorting options.

---
+++ Features/FileManager/src/main/java/com/features/file/manager/utils/Constants.kt
@@ -0,0 +1,24 @@
+package com.features.file.manager.utils
+
+// sorting
+const val SORT_ORDER = "sort_order"
+const val SORT_FOLDER_PREFIX = "sort_folder_"       // storing folder specific values at using "Use for this folder only"
+const val SORT_BY_NAME = 1
+const val SORT_BY_DATE_MODIFIED = 2
+const val SORT_BY_SIZE = 4
+const val SORT_BY_DATE_TAKEN = 8
+const val SORT_BY_EXTENSION = 16
+const val SORT_BY_PATH = 32
+const val SORT_BY_NUMBER = 64
+const val SORT_BY_FIRST_NAME = 128
+const val SORT_BY_MIDDLE_NAME = 256
+const val SORT_BY_SURNAME = 512
+const val SORT_DESCENDING = 1024
+const val SORT_BY_TITLE = 2048
+const val SORT_BY_ARTIST = 4096
+const val SORT_BY_DURATION = 8192
+const val SORT_BY_RANDOM = 16384
+const val SORT_USE_NUMERIC_VALUE = 32768
+const val SORT_BY_FULL_NAME = 65536
+const val SORT_BY_CUSTOM = 131072
+const val SORT_BY_DATE_CREATED = 262144
