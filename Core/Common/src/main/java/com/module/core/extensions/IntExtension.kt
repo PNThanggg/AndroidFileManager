c6e43e09d2cc52504aaa16d367279ddd1eb290ec
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-03-03 16:59:17 +0700

    :sparkles: Add `getFormattedDuration` extension function to format seconds into HH:MM:SS && :recycle: Refactor `removeBit` extension function in `IntExtension`

--- Core/Common/src/main/java/com/module/core/extensions/IntExtension.kt
+++ Core/Common/src/main/java/com/module/core/extensions/IntExtension.kt
@@ -1,6 +1,7 @@
 package com.module.core.extensions
 
 import android.graphics.Color
+import java.util.Locale
 
 val DARK_GREY = 0xFF333333.toInt()
 
@@ -21,3 +22,20 @@ fun Int.addBit(bit: Int) = this or bit
 
 // TODO: how to do "bits & ~bit" in kotlin?
 fun Int.removeBit(bit: Int) = addBit(bit) - bit
+
+fun Int.getFormattedDuration(forceShowHours: Boolean = false): String {
+    val sb = StringBuilder(8)
+    val hours = this / 3600
+    val minutes = this % 3600 / 60
+    val seconds = this % 60
+
+    if (this >= 3600) {
+        sb.append(String.format(Locale.getDefault(), "%02d", hours)).append(":")
+    } else if (forceShowHours) {
+        sb.append("0:")
+    }
+
+    sb.append(String.format(Locale.getDefault(), "%02d", minutes))
+    sb.append(":").append(String.format(Locale.getDefault(), "%02d", seconds))
+    return sb.toString()
+}
