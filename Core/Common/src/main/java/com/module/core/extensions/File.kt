c95db683d78608203b653a0c138e366c2f39614d
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-03-04 01:01:13 +0700

    :sparkles: Add `getProperSize` and `getDirectChildrenCount` extensions to File

--- Core/Common/src/main/java/com/module/core/extensions/File.kt
+++ Core/Common/src/main/java/com/module/core/extensions/File.kt
@@ -62,3 +62,46 @@ fun File.deleteFiles() {
 val File.prettyName: String
     get() = this.name.takeIf { this.path != Environment.getExternalStorageDirectory()?.path }
         ?: "Internal Storage"
+
+private fun getDirectorySize(dir: File, countHiddenItems: Boolean): Long {
+    var size = 0L
+    if (dir.exists()) {
+        val files = dir.listFiles()
+        if (files != null) {
+            for (i in files.indices) {
+                if (files[i].isDirectory) {
+                    size += getDirectorySize(files[i], countHiddenItems)
+                } else if (!files[i].name.startsWith('.') && !dir.name.startsWith('.') || countHiddenItems) {
+                    size += files[i].length()
+                }
+            }
+        }
+    }
+    return size
+}
+
+fun File.getProperSize(countHiddenItems: Boolean): Long {
+    return if (isDirectory) {
+        getDirectorySize(this, countHiddenItems)
+    } else {
+        length()
+    }
+}
+
+fun File.getDirectChildrenCount(context: Context, countHiddenItems: Boolean): Int {
+    val fileCount = if (context.isRestrictedSAFOnlyRoot(path)) {
+        context.getAndroidSAFDirectChildrenCount(
+            path, countHiddenItems
+        )
+    } else {
+        listFiles()?.filter {
+            if (countHiddenItems) {
+                true
+            } else {
+                !it.name.startsWith('.')
+            }
+        }?.size ?: 0
+    }
+
+    return fileCount
+}
