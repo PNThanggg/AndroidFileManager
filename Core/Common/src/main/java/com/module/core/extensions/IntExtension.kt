2c4eb9b074ac7b9311f84c6722fdeecc55cf76b0
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-03-03 11:28:38 +0700

    :sparkles: Add contrast color calculation and alpha adjustment to `Int` extension.

---
+++ Core/Common/src/main/java/com/module/core/extensions/IntExtension.kt
@@ -0,0 +1,18 @@
+package com.module.core.extensions
+
+import android.graphics.Color
+
+val DARK_GREY = 0xFF333333.toInt()
+
+fun Int.getContrastColor(): Int {
+    val y = (299 * Color.red(this) + 587 * Color.green(this) + 114 * Color.blue(this)) / 1000
+    return if (y >= 149 && this != Color.BLACK) DARK_GREY else Color.WHITE
+}
+
+fun Int.adjustAlpha(factor: Float): Int {
+    val alpha = Math.round(Color.alpha(this) * factor)
+    val red = Color.red(this)
+    val green = Color.green(this)
+    val blue = Color.blue(this)
+    return Color.argb(alpha, red, green, blue)
+}
