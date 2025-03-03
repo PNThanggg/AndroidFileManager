c0fb8ba031a11cf676a7f151f0780fbaedc28367
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-03-03 17:20:16 +0700

    :sparkles: Add Root support to FileManager

--- Features/FileManager/src/main/java/com/features/file/manager/FileManagerActivity.kt
+++ Features/FileManager/src/main/java/com/features/file/manager/FileManagerActivity.kt
@@ -2,7 +2,10 @@ package com.features.file.manager
 
 import android.view.LayoutInflater
 import com.features.file.manager.databinding.ActivityFileManagerBinding
+import com.features.file.manager.utils.RootHelpers
+import com.library.root_tools.RootTools
 import com.module.core.base.BaseActivity
+import com.module.core.ensureBackgroundThread
 
 class FileManagerActivity : BaseActivity<ActivityFileManagerBinding>() {
     override fun inflateViewBinding(inflater: LayoutInflater): ActivityFileManagerBinding {
@@ -14,4 +17,15 @@ class FileManagerActivity : BaseActivity<ActivityFileManagerBinding>() {
     override fun initData() {}
 
     override fun initListener() {}
+
+    private fun checkIfRootAvailable() {
+        ensureBackgroundThread {
+            config.isRootAvailable = RootTools.isRootAvailable()
+            if (config.isRootAvailable && config.enableRootAccess) {
+                RootHelpers(this).askRootIfNeeded {
+                    config.enableRootAccess = it
+                }
+            }
+        }
+    }
 }
